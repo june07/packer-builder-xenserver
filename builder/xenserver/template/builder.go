ce613a221cb66bed4476f4c00b6441cf12c32ed5
Will <will@onezerohosting.com>
2015-09-16 08:58:55 -0700

    Modified iso builder to convert VM image into a template Added template builder to create VMs on the XenServer host from templates

---
+++ builder/xenserver/template/builder.go
@@ -0,0 +1,239 @@
+package template
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/mitchellh/multistep"
+	"github.com/mitchellh/packer/common"
+	"github.com/mitchellh/packer/helper/communicator"
+	hconfig "github.com/mitchellh/packer/helper/config"
+	"github.com/mitchellh/packer/packer"
+	"github.com/mitchellh/packer/template/interpolate"
+	xscommon "github.com/rdobson/packer-builder-xenserver/builder/xenserver/common"
+	xsclient "github.com/xenserver/go-xenserver-client"
+)
+
+type config struct {
+	common.PackerConfig   `mapstructure:",squash"`
+	xscommon.CommonConfig `mapstructure:",squash"`
+
+	VMMemory      uint   `mapstructure:"vm_memory"`
+	DiskSize      uint   `mapstructure:"disk_size"`
+	CloneTemplate string `mapstructure:"clone_template"`
+
+	PlatformArgs map[string]string `mapstructure:"platform_args"`
+
+	RawInstallTimeout string        `mapstructure:"install_timeout"`
+	InstallTimeout    time.Duration ``
+
+	ctx interpolate.Context
+}
+
+type Builder struct {
+	config config
+	runner multistep.Runner
+}
+
+func (self *Builder) Prepare(raws ...interface{}) (params []string, retErr error) {
+
+	var errs *packer.MultiError
+
+	err := hconfig.Decode(&self.config, &hconfig.DecodeOpts{
+		Interpolate: true,
+		InterpolateFilter: &interpolate.RenderFilter{
+			Exclude: []string{
+				"boot_command",
+			},
+		},
+	}, raws...)
+
+	if err != nil {
+		packer.MultiErrorAppend(errs, err)
+	}
+
+	errs = packer.MultiErrorAppend(
+		errs, self.config.CommonConfig.Prepare(&self.config.ctx, &self.config.PackerConfig)...)
+	errs = packer.MultiErrorAppend(errs, self.config.SSHConfig.Prepare(&self.config.ctx)...)
+
+	// Set default values
+
+	if self.config.RawInstallTimeout == "" {
+		self.config.RawInstallTimeout = "200m"
+	}
+
+	if self.config.DiskSize == 0 {
+		self.config.DiskSize = 40000
+	}
+
+	if self.config.VMMemory == 0 {
+		self.config.VMMemory = 1024
+	}
+
+	if self.config.CloneTemplate == "" {
+		self.config.CloneTemplate = "Other install media"
+	}
+
+	if len(self.config.PlatformArgs) == 0 {
+		pargs := make(map[string]string)
+		pargs["viridian"] = "false"
+		pargs["nx"] = "true"
+		pargs["pae"] = "true"
+		pargs["apic"] = "true"
+		pargs["timeoffset"] = "0"
+		pargs["acpi"] = "1"
+		self.config.PlatformArgs = pargs
+	}
+
+	// Validation
+
+	self.config.InstallTimeout, err = time.ParseDuration(self.config.RawInstallTimeout)
+	if err != nil {
+		errs = packer.MultiErrorAppend(
+			errs, fmt.Errorf("Failed to parse install_timeout: %s", err))
+	}
+
+	return nil, retErr
+
+}
+
+func (self *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packer.Artifact, error) {
+	//Setup XAPI client
+	client := xsclient.NewXenAPIClient(self.config.HostIp, self.config.Username, self.config.Password)
+
+	err := client.Login()
+	if err != nil {
+		return nil, err.(error)
+	}
+	ui.Say("XAPI client session established")
+
+	client.GetHosts()
+
+	//Share state between the other steps using a statebag
+	state := new(multistep.BasicStateBag)
+	state.Put("cache", cache)
+	state.Put("client", client)
+	state.Put("config", self.config)
+	state.Put("commonconfig", self.config.CommonConfig)
+	state.Put("hook", hook)
+	state.Put("ui", ui)
+
+	httpReqChan := make(chan string, 1)
+
+	//Build the steps
+
+	steps := []multistep.Step{
+		&xscommon.StepPrepareOutputDir{
+			Force: self.config.PackerForce,
+			Path:  self.config.OutputDir,
+		},
+		&common.StepCreateFloppy{
+			Files: self.config.FloppyFiles,
+		},
+		&xscommon.StepHTTPServer{
+			Chan: httpReqChan,
+		},
+		&xscommon.StepUploadVdi{
+			VdiNameFunc: func() string {
+				return "Packer-floppy-disk"
+			},
+			ImagePathFunc: func() string {
+				if floppyPath, ok := state.GetOk("floppy_path"); ok {
+					return floppyPath.(string)
+				}
+				return ""
+			},
+			VdiUuidKey: "floppy_vdi_uuid",
+		},
+		&xscommon.StepFindVdi{
+			VdiName:    self.config.ToolsIsoName,
+			VdiUuidKey: "tools_vdi_uuid",
+		},
+		new(stepCreateInstance),
+		&xscommon.StepAttachVdi{
+			VdiUuidKey: "floppy_vdi_uuid",
+			VdiType:    xsclient.Floppy,
+		},
+		&xscommon.StepAttachVdi{
+			VdiUuidKey: "tools_vdi_uuid",
+			VdiType:    xsclient.CD,
+		},
+		new(xscommon.StepStartVmPaused),
+		new(xscommon.StepGetVNCPort),
+		&xscommon.StepForwardPortOverSSH{
+			RemotePort:  xscommon.InstanceVNCPort,
+			RemoteDest:  xscommon.InstanceVNCIP,
+			HostPortMin: self.config.HostPortMin,
+			HostPortMax: self.config.HostPortMax,
+			ResultKey:   "local_vnc_port",
+		},
+		new(xscommon.StepBootWait),
+		&xscommon.StepTypeBootCommand{
+			Ctx: self.config.ctx,
+		},
+		&xscommon.StepWaitForIP{
+			Chan:    httpReqChan,
+			Timeout: self.config.InstallTimeout, // @todo change this
+		},
+		&xscommon.StepForwardPortOverSSH{
+			RemotePort:  xscommon.InstanceSSHPort,
+			RemoteDest:  xscommon.InstanceSSHIP,
+			HostPortMin: self.config.HostPortMin,
+			HostPortMax: self.config.HostPortMax,
+			ResultKey:   "local_ssh_port",
+		},
+		&communicator.StepConnect{
+			Config:    &self.config.SSHConfig.Comm,
+			Host:      xscommon.CommHost,
+			SSHConfig: xscommon.SSHConfigFunc(self.config.CommonConfig.SSHConfig),
+			SSHPort:   xscommon.SSHPort,
+		},
+		new(xscommon.StepShutdown),
+		&xscommon.StepDetachVdi{
+			VdiUuidKey: "floppy_vdi_uuid",
+		},
+		new(xscommon.StepStartVmPaused),
+		new(xscommon.StepBootWait),
+		&communicator.StepConnectSSH{
+			Config:    &self.config.SSHConfig.Comm,
+			Host:      xscommon.CommHost,
+			SSHConfig: xscommon.SSHConfigFunc(self.config.CommonConfig.SSHConfig),
+			SSHPort:   xscommon.SSHPort,
+		},
+		new(common.StepProvision),
+		new(xscommon.StepShutdown),
+		&xscommon.StepDetachVdi{
+			VdiUuidKey: "tools_vdi_uuid",
+		},
+		new(xscommon.StepExport),
+	}
+
+	self.runner = &multistep.BasicRunner{Steps: steps}
+	self.runner.Run(state)
+
+	if rawErr, ok := state.GetOk("error"); ok {
+		return nil, rawErr.(error)
+	}
+
+	// If we were interrupted or cancelled, then just exit.
+	if _, ok := state.GetOk(multistep.StateCancelled); ok {
+		return nil, errors.New("Build was cancelled.")
+	}
+	if _, ok := state.GetOk(multistep.StateHalted); ok {
+		return nil, errors.New("Build was halted.")
+	}
+
+	artifact, _ := xscommon.NewArtifact(self.config.OutputDir)
+
+	return artifact, nil
+}
+
+func (self *Builder) Cancel() {
+	if self.runner != nil {
+		log.Println("Cancelling the step runner...")
+		self.runner.Cancel()
+	}
+	fmt.Println("Cancelling the builder")
+}
